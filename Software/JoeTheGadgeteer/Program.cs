a0f7194995329d33b5dae27010a4971328222f5d
Ian Lee <ian@houseoflees.net>
2013-08-16 21:25:54 -0500

    Added heartbeat.

---
+++ Software/JoeTheGadgeteer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+using GTI = Gadgeteer.Interfaces;
+
+namespace JoeTheGadgeteer
+{
+    public partial class Program
+    {
+        private GTI.DigitalOutput _heartPin;
+
+        void ProgramStarted()
+        {
+            Debug.Print("Program Started");
+
+            _heartPin = upperServos.SetupDigitalOutput(GT.Socket.Pin.Three, false);
+
+            var heartTimer = new GT.Timer(1000);
+            heartTimer.Tick += t =>
+            {
+                // Blink the LED (heart).
+                _heartPin.Write(true);
+                Thread.Sleep(200);
+                _heartPin.Write(false);
+            };
+            heartTimer.Start();
+        }
+    }
+}
